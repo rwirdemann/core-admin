1138d7885f44f22c2485face62ebea6dc6d0cea2
Ralf Wirdemann <ralf.wirdemann@gmail.com>
2019-07-02 08:31:16 +0200

    Initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <reach></reach>
+  </div>
+</template>
+
+<script>
+import reach from './components/reach'
+
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "REACH Web UI"
+    };
+  }
+};
+</script>
---
+++ src/components/reach.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+
+Vue.component('reach', {
+  template: '<h1>Models</h1>'
+})
