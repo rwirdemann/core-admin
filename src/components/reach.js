81ded37025ece6a242e55642037f8770799b36e8
Ralf Wirdemann <ralf.wirdemann@gmail.com>
2019-07-02 10:52:30 +0200

    Fetch substances

--- src/components/reach.js
+++ src/components/reach.js
@@ -9,6 +9,16 @@ Vue.component('reach', {
           {{ vin.vin_year }} 
         </option>
       </select>
+      <select v-model="selectedModel" v-on:change="fetchSubstances()">
+        <option v-for="model in models">
+          {{ model.model_name }}
+          {{ model.base_model }} 
+          {{ model.model_year }} 
+        </option>
+      </select>
+      <ul>
+        <li v-for="s in substances">{{ s.TeivonDE }} {{ s.CAS }} {{ s.NameDE }}</li>
+      </ul>
     </div>
   `,
   data() {
@@ -35,4 +45,23 @@ Vue.component('reach', {
       })
       .catch((err) => console.error(err))
   },
+  methods: {
+    loadModels: function () {
+      this.models = this.vinMap[this.selected.trim()]
+      this.substances = []
+    },
+    fetchSubstances: function (event) {
+      var a = this.selectedModel.trim().split(" ");
+      var baseModel = a[a.length - 2].trim()
+      var modelYear = a[a.length - 1].trim()
+      var url = "https://" + this.baseUrl + "/api/v2/substances/" + baseModel + "/" + modelYear
+      fetch(url)
+        .then(response => response.json())
+        .then(json => {
+          this.substances = json.vehicle.reach
+          console.log(json)
+        })
+        .catch((err) => console.error(err))
+    }
+  }
 })
